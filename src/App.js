4a2c5ec3da73cfd57cd2f792a79fd02fe40d521c
eddym <eddyats11"gmail.com>
2021-07-30 23:06:03 +0300

    Continuation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+import Dashboard from "./components/Dashboard";
+import Preferences from "./components/Preferences";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { useState } from 'react';
+import Login from "./components/Login";
 
 function App() {
+    const [token, setToken] = useState();
+    if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div>
-Hello World!!!
-</div>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
